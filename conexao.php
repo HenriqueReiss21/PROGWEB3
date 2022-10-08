9f42dd2365a76113de3bcfc94d5b8ca3b0e4dc81
HenriqueReiss21 <85037643+HenriqueReiss21@users.noreply.github.com>
2022-10-08 15:56:01 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-$user = "adminprogweb";
+$user = "adminprogweb"; // Para que o código seja funcional, é preciso criar no phpmyadmin o usuário adminprogweb, com a senha ProgWeb3
 $pass = "ProgWeb3";
-$db = "progweb3";
+$db = "progweb3"; // É necessário criar um banco de dados com o nome progweb3 e, após isso, importar o arquivo progweb3.sql, que contém as tabelas e valores necessários
 $conn = mysqli_connect("localhost", $user, $pass, $db);
 if ($conn->connect_errno){
     die("Erro de conexão" . $conn->connect_error); //Se a conexao falhar, vai mandar uma msg de erro
